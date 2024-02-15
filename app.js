04853e05644f7cf624472570c1bd25c55dcd5054
unknown <yusuff4u2c@gmail.com>
2024-02-15 09:25:21 +0100

    env configuration updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: "http://localhost:5173",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
